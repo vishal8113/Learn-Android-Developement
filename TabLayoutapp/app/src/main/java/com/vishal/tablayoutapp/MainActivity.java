a84125f5674c55f7c9842f672636ab5475757184
Vishal Chauhan <vsc258srt125vv@gmail.com>
2023-05-21 16:30:53 +0530

    Implemented Tab Layout Functionality

---
+++ TabLayoutapp/app/src/main/java/com/vishal/tablayoutapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.vishal.tablayoutapp;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import android.os.Bundle;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(viewPagerAdapter);
+
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
